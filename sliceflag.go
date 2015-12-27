c78871c8bf3b0ae556db515110b77687ceb129b3
soh335 <sugarbabe335@gmail.com>
2015-12-27 16:55:20 +0900

    duration func

--- sliceflag.go
+++ sliceflag.go
@@ -3,7 +3,39 @@ package sliceflag
 import (
 	"flag"
 	"fmt"
+	"time"
 )
+
+type durationSliceValue struct {
+	p     *[]time.Duration
+	seted bool
+}
+
+func newDurationSliceValue(val []time.Duration, p *[]time.Duration) *durationSliceValue {
+	*p = append(*p, val...)
+	return &durationSliceValue{p, false}
+}
+
+func (d *durationSliceValue) Set(value string) error {
+	v, err := time.ParseDuration(value)
+	if err != nil {
+		return err
+	}
+	if d.seted == false && len(*d.p) > 0 {
+		*d.p = (*d.p)[:0]
+	}
+	*d.p = append(*d.p, v)
+	d.seted = true
+	return nil
+}
+
+func (d *durationSliceValue) String() string {
+	return fmt.Sprintf("%v", *d.p)
+}
+
+func (d *durationSliceValue) Get() interface{} {
+	return *d.p
+}
 
 type stringSliceValue struct {
 	p     *[]string
@@ -31,6 +63,16 @@ func (s *stringSliceValue) String() string {
 
 func (s *stringSliceValue) Get() interface{} {
 	return *s.p
+}
+
+func Duration(flagset *flag.FlagSet, name string, value []time.Duration, usage string) *[]time.Duration {
+	p := new([]time.Duration)
+	DurationVar(flagset, p, name, value, usage)
+	return p
+}
+
+func DurationVar(flagset *flag.FlagSet, p *[]time.Duration, name string, value []time.Duration, usage string) {
+	flagset.Var(newDurationSliceValue(value, p), name, usage)
 }
 
 func String(flagset *flag.FlagSet, name string, value []string, usage string) *[]string {
--- sliceflag_test.go
+++ sliceflag_test.go
@@ -5,24 +5,35 @@ import (
 	"flag"
 	"reflect"
 	"testing"
+	"time"
 )
 
-func TestString(t *testing.T) {
+func TestAll(t *testing.T) {
 	flagset := flag.NewFlagSet("test", flag.ContinueOnError)
+
 	stringFlag1 := String(flagset, "string1", []string{}, "string1 value")
 	stringFlag2 := String(flagset, "string2", []string{"ddd"}, "string2 value")
 	stringFlag3 := String(flagset, "string3", []string{"eee"}, "string3 value")
+
+	durationFlag1 := Duration(flagset, "duration1", []time.Duration{}, "duration1 value")
+	durationFlag2 := Duration(flagset, "duration2", []time.Duration{time.Second}, "duration2 value")
+	durationFlag3 := Duration(flagset, "duration3", []time.Duration{time.Minute}, "duration3 value")
 	args := []string{
 		"-string1", "aaa",
-		"-string2", "ccc",
 		"-string1", "bbb",
+		"-string2", "ccc",
+		"-duration1", "1s",
+		"-duration1", "10s",
+		"-duration2", "10m",
 	}
+
 	if err := flagset.Parse(args); err != nil {
 		t.Fatal(err)
 	}
 	if !flagset.Parsed() {
 		t.Error("flagset.Parsed() = false after Parse")
 	}
+
 	if e, g := []string{"aaa", "bbb"}, *stringFlag1; !reflect.DeepEqual(e, g) {
 		t.Errorf("stringFlag1 expected %v got %v", e, g)
 	}
@@ -33,11 +44,27 @@ func TestString(t *testing.T) {
 		t.Errorf("stringFlag3 expected %v got %v", e, g)
 	}
 
+	if e, g := []time.Duration{time.Second, time.Second * 10}, *durationFlag1; !reflect.DeepEqual(e, g) {
+		t.Errorf("durationFlag1 expected %v got %v", e, g)
+	}
+	if e, g := []time.Duration{time.Minute * 10}, *durationFlag2; !reflect.DeepEqual(e, g) {
+		t.Errorf("durationFlag2 expected %v got %v", e, g)
+	}
+	if e, g := []time.Duration{time.Minute}, *durationFlag3; !reflect.DeepEqual(e, g) {
+		t.Errorf("durationFlag3 expected %v got %v", e, g)
+	}
+
 	var b bytes.Buffer
 	flagset.SetOutput(&b)
 	flagset.PrintDefaults()
 
-	if e, g := `  -string1 value
+	if e, g := `  -duration1 value
+    	duration1 value (default [])
+  -duration2 value
+    	duration2 value (default [1s])
+  -duration3 value
+    	duration3 value (default [1m0s])
+  -string1 value
     	string1 value (default [])
   -string2 value
     	string2 value (default [ddd])
@@ -47,6 +74,9 @@ func TestString(t *testing.T) {
 		t.Errorf("defaults expected %v got %v", e, g)
 	}
 
+	if e, g := []time.Duration{time.Second, time.Second * 10}, flagset.Lookup("duration1").Value.(flag.Getter).Get(); !reflect.DeepEqual(e, g) {
+		t.Errorf("duration1 lookup expected %v got %v", e, g)
+	}
 	if e, g := []string{"aaa", "bbb"}, flagset.Lookup("string1").Value.(flag.Getter).Get(); !reflect.DeepEqual(e, g) {
 		t.Errorf("string1 lookup expected %v got %v", e, g)
 	}
